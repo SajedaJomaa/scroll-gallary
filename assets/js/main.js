8fd1abfdf2c72af470f8c099103cbfa223bd00fb
SajedaJomaa <sajedajomaa57@gmail.com>
2024-04-29 00:48:43 +0300

    update

--- assets/js/main.js
+++ assets/js/main.js
@@ -19,17 +19,22 @@ async function fetchData() {
         let div = document.createElement('div');
         div.className = 'image';
         div.innerHTML = `<img src="${image.download_url}" alt="">`
+        document.querySelector('.imageId').innerHTML = image.id;
+
         root.appendChild(div);
         document.querySelectorAll('.image-container .image img').forEach(image => {
             image.onclick = () => {
                 document.querySelector('.popup-image').style.display = 'block';
                 document.querySelector('.popup-image img').src = image.getAttribute('src');
-        
+                let btnDownload = document.querySelector('.downloadImage');
+                btnDownload.addEventListener('click', () => {
+                });
+
             }
         });
         document.querySelector('.popup-image span').onclick = () => {
-            document.querySelector('.popup-image').style.display = 'none';
-        
+            document.querySelector('.popup-image ').style.display = 'none';
+
         }
     }
     currentPage++;
